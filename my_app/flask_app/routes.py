e6ccdda169f27bef103ba7d0291af1c77356207e
Dan Jacobsen <dan@danjacobsen.com>
2023-09-19 19:19:33 -0700

    rework with swagger

--- my_app/flask_app/routes.py
+++ my_app/flask_app/routes.py
@@ -1,8 +1,47 @@
-from flask import Blueprint
+from flask_restx import Resource, fields, Namespace
+from typing import List, Dict
 
-hello_world_bp = Blueprint("hello_world", __name__)
+api: Namespace = Namespace("hello_world", description="The Hello World namespace")
+
+world: Dict[str, fields.String] = api.model(
+    "World",
+    {
+        "id": fields.String(required=True, description="The world identifier"),
+        "name": fields.String(required=True, description="The world name"),
+    },
+)
+
+WORLDS: List[Dict[str, str]] = [
+    {"id": "home", "name": "Earth"},
+]
 
 
-@hello_world_bp.route("/")
-def hello_world():
-    return "Hello, World!"
+@api.route("/hello_world")
+class HelloWorld(Resource):
+    @api.doc("hello_world")
+    def get(self) -> Dict[str, str]:
+        return {"hello": "world"}
+
+
+@api.route("/hello_all_worlds")
+class WorldList(Resource):
+    @api.doc("list_worlds")
+    @api.marshal_list_with(world)
+    def get(self) -> List[Dict[str, str]]:
+        """List all worlds"""
+        return WORLDS
+
+
+@api.route("/<id>")
+@api.param("id", "The world identifier")
+@api.response(404, "World not found")
+class World(Resource):
+    @api.doc("get_world")
+    @api.marshal_with(world)
+    def get(self, id: str) -> Dict[str, str]:
+        """Fetch a world given its identifier"""
+        for world in WORLDS:
+            if world["id"] == id:
+                return world
+        api.abort(404)
+        return {}
