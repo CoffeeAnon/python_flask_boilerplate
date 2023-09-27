474736b0db3a5e13dffaea94da0db2d02efd8d27
Dan Jacobsen <dan@danjacobsen.com>
2023-09-27 12:39:43 -0700

    test refactor - data -> json

--- my_app/tests/test_routes.py
+++ my_app/tests/test_routes.py
@@ -1,7 +1,7 @@
 def test_hello_world(client):
     response = client.get("/hello_world/hello_world")
     assert response.status_code == 200
-    assert response.data == b'{"hello": "world"}\n'
+    assert response.json == {"hello": "world"}
 
 
 def test_hello_all_worlds(client):
