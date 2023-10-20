bfd35de0e3b68628235562923afbe81284b68007
Dan Jacobsen <dan@danjacobsen.com>
2023-10-20 04:08:45 +0000

    remove create table for alembic

--- bytebase/dev/boilerplate_dev##8a1c72f3bd0a1##ddl##create_messages_worlds.sql
+++ bytebase/dev/boilerplate_dev##8a1c72f3bd0a1##ddl##create_messages_worlds.sql
@@ -1,10 +1,5 @@
 BEGIN;
 
-CREATE TABLE alembic_version (
-    version_num VARCHAR(32) NOT NULL, 
-    CONSTRAINT alembic_version_pkc PRIMARY KEY (version_num)
-);
-
 -- Running upgrade  -> 8a1c72f3bd0a
 
 CREATE TABLE messages (
